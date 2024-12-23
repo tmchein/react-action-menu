b8aac1f7060ca0e3a8aa2d21e9f28915dc24be0e
Juan rojas <davidr2998@gmail.com>
2024-12-23 15:45:30 -0500

    separating types from context

--- src/shared/context.tsx
+++ src/shared/context.tsx
@@ -1,23 +1,13 @@
-import { createContext, Dispatch, SetStateAction, useState } from "react";
+import { createContext, useState } from "react";
 import { ActionMenuOption } from "../components/ActionMenu";
-
-type ActionMenuContextType = {
-  selectedOption: ActionMenuOption | null | undefined;
-  setSelectedOption: Dispatch<
-    SetStateAction<ActionMenuOption | null | undefined>
-  >;
-};
-
-type contextProps = {
-  children: React.ReactElement;
-};
+import { ActionMenuContextProps, ActionMenuContextType } from "./types";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const ActionMenuContext = createContext<ActionMenuContextType | null>(
   null,
 );
 
-const ActionMenuContextProvider = ({ children }: contextProps) => {
+const ActionMenuContextProvider = ({ children }: ActionMenuContextProps) => {
   const [selectedOption, setSelectedOption] =
     useState<ActionMenuOption | null>();
 
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,21 @@
-export type GenericOption = {
+import { Dispatch, SetStateAction } from "react";
+import { ActionMenuOption } from "../components/ActionMenu";
+
+type GenericOption = {
   id: string;
   label: string;
   value: string;
 };
+
+type ActionMenuContextType = {
+  selectedOption: ActionMenuOption | null | undefined;
+  setSelectedOption: Dispatch<
+    SetStateAction<ActionMenuOption | null | undefined>
+  >;
+};
+
+type ActionMenuContextProps = {
+  children: React.ReactElement;
+};
+
+export type { GenericOption, ActionMenuContextProps, ActionMenuContextType };
